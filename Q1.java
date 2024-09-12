e2c8eaadd9ca9a663205912482a44adcbd032bf9
Somnath Londhe <somnathlondhe1658@gmail.com>
2024-09-12 14:18:31 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,9 @@
+//Write a ‘java’ program to display characters from ‘A’ to ‘Z’.
+public class Q1 {
+    public static void main(String[] args) {
+    // char ;
+    for (char i = 'A'; i <= 'Z'; i++){
+        System.out.print(i+" ");
+    }
+    }
+}
